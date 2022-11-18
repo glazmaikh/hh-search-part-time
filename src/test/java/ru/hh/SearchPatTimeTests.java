72a58611ef7d4ead5a5b848dae45060be5dc40ac
Sergey Orlovskiy <sergey.orlovskiy667@gmail.com>
2022-11-18 12:30:20 +0300

    create setUp in @BeforeAll and test open site

--- src/test/java/ru/hh/SearchPatTimeTests.java
+++ src/test/java/ru/hh/SearchPatTimeTests.java
@@ -1,4 +1,24 @@
 package ru.hh;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class SearchPatTimeTests {
+
+    @BeforeAll
+    static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C://webdrivers/chromedriver.exe");
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1932x1160";
+        Configuration.browserPosition = "-6x0";
+        Configuration.baseUrl = "https://hh.ru";
+    }
+
+    @Test
+    void fillSearchFormTest() {
+        open("/search/vacancy/advanced");
+    }
 }
